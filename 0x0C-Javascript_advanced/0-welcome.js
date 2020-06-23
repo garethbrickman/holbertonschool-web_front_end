66bc4fec78f29eff08201b9da81017bc1871873f
Gareth Brickman <brickstick@gmail.com>
2020-06-23 15:54:36 -0400

    Add 0.

--- 0x0C-Javascript_advanced/0-welcome.js
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+function welcome(firstName, lastName) {
+
+	const fullName = firstName + ' ' + lastName;
+	
+	function displayFullName() {
+		alert('Welcome ' + fullName + '!');
+	}
+	displayFullName();
+}
