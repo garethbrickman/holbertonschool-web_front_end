3f1623b9bb127beec2109dc64a2ee20f7c8a9471
Gareth Brickman <brickstick@gmail.com>
2020-06-29 01:50:23 -0400

    Incl actual prime algo

--- 0x0C-Javascript_advanced/10-prime.js
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -1,5 +1,23 @@
 function countPrimeNumbers() {
-	return 25
+	let primes=0; 
+	let isprime = true;
+	for (let counter = 2; counter <= 100; counter++)
+	{
+		isprime = true;
+		let limit = Math.round(Math.sqrt(counter)); 
+		for (let mod = 2; mod <= limit; mod++) {
+			if (counter%mod == 0) {
+				isprime = false;
+				break; 
+			}
+		}
+		if (isprime) {
+			primes = primes + 1;
+		}
+
+	}
+	console.log(primes);
+	return primes;
 }
 
 let startTime = window.performance.now();
