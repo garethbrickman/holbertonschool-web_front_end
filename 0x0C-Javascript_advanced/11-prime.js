c3b81f660afe3501ede85c95e1de484db7d7272f
Gareth Brickman <brickstick@gmail.com>
2020-06-29 01:56:13 -0400

    Add 11.

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+	let primes=0; 
+	let isprime = true;
+	for (let counter = 2; counter <= 100; counter++)
+	{
+		isprime = true;
+		let limit = Math.round(Math.sqrt(counter)); 
+		for (let mod = 2; mod <= limit; mod++) {
+			if (counter%mod == 0) {
+				isprime = false;
+				break; 
+			}
+		}
+		if (isprime) {
+			primes = primes + 1;
+		}
+
+	}
+	console.log(primes);
+	return primes;
+}
+
+let startTime = window.performance.now();
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+let endTime = window.performance.now();
+setTimeout (function() { console.log("Execution time of calculating prime numbers 100 times was " + (endTime - startTime)); }, 0);
